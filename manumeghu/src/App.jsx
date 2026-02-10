60b51d9c5049138b0f267adbaeb64d505afcabaf
xyz <xyz@gmail.com>
2026-02-10 16:12:42 +0530

    files are changed

--- manumeghu/src/App.jsx
+++ manumeghu/src/App.jsx
@@ -4,15 +4,16 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setText] = useState("")
+  const [text, setText] = useState("")
 
   return (
-<>
+    <>
+      <div>
+        <input value={text} onchange={(e) => setText(e.target.value)}/>
+        <p>{text}</p>
+      </div>
     
-</>
-
-
-  )
-     
+    </>
+)
 }
 export default App
